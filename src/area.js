ba0331f9fa638995cb9ed8c896b3e095d1614148
ParasArya289 <parasarya289@gmail.com>
2022-12-23 23:29:29 +0530

    fix: added error handling in area

--- src/area.js
+++ src/area.js
@@ -1,24 +1,37 @@
 import { useState } from "react";
 
-let Area = () =>{
-    let [inp,setInp] = useState('');
-    let [area,setArea] = useState();
-    let [base,height] = inp.split(',');
+let Area = () => {
+  let [inp, setInp] = useState("");
+  let [area, setArea] = useState();
+  let [base, height] = inp.split(",");
 
-    // console.log(+base,+height)
-
-    let calculateArea = () =>{
-        let a = ((+base) * (+height))/2
-        setArea(a);
+  let calculateArea = () => {
+    if (base && height && base > 0 && height > 0) {
+      let a = (+base * +height) / 2;
+      setArea(`Area formed by ${base} and ${height} is ${a} cm²`);
+    } else {
+      alert(
+        "Please follow format (Base,Height) dont forget commas and provide positive values"
+      );
     }
-    return(
-        <div>
-            <h1 className="heading">Area of <span>Triangle</span></h1>
-            <h3 className="heading">1/2 x Base x Height</h3>
-            <input className="input-angle" type="text" onChange={e=>setInp(e.target.value)} placeholder="Format: Base,Height" />
-            <button className="btn-istriangle" onClick={calculateArea}>Check</button>
-            <h2 className="heading">Area formed by {base} and {height} is {area} cm2</h2>
-        </div>
-    )
-}
+  };
+  return (
+    <div>
+      <h1 className="heading">
+        Area of <span>Triangle</span>
+      </h1>
+      <h3 className="heading">1/2 x Base x Height</h3>
+      <input
+        className="input-angle"
+        type="text"
+        onChange={(e) => setInp(e.target.value)}
+        placeholder="Format: Base,Height"
+      />
+      <button className="btn-istriangle" onClick={calculateArea}>
+        Check
+      </button>
+      <h2 className="heading">{area}</h2>
+    </div>
+  );
+};
 export default Area;
